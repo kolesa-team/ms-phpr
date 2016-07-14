dda31b79bc59be645405b076dcec9c3ff03ae959
Igor Borodikhin <iborodikhin@gmail.com>
2016-07-14 12:27:51 +0600

    Версия

--- src/main.go
+++ src/main.go
@@ -27,7 +27,7 @@ func main() {
 
 	app.Name = "phpr"
 	app.Usage = "Photo proxy with resizing and watermarks"
-	app.Version = "0.1.0"
+	app.Version = "0.1.1"
 	app.Author = "Igor Borodikhin"
 	app.Email = "iborodikhin@gmail.com"
 	app.Action = actionRun
